800f2e6d72f8a0993a3bb88a5b4eb19ecd8e70ad
ROUA_AFSA <rouaafsa@gmail.com>
2025-12-01 12:21:03 +0100

    auth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -22,6 +25,23 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token,string $firewallName): Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        $user = $token->getUser();
+        if(in_array('ROLE_CLIENT', $user->getRoles(),true)){
+            return new RedirectResponse($this->generate('app_voiture'));
+        }
+
+        if(in_array('ROLE_AGENT', $user->getRoles(),true)){
+            return new RedirectResponse($this->urlGenerator->generate('app_client_index'));
+        }
+        return new RedirectResponse($this->urlGenerator->generate('app_location_new'));
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
